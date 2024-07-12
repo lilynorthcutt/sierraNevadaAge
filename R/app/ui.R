7dbf57a13e6db2eeacf235b89d851f566c78200c
lilynorthcutt <northcuttlilya@gmail.com>
2024-07-12 14:37:09 -0400

    Improved UI to use accordion for folding

--- R/app/ui.R
+++ R/app/ui.R
@@ -1,25 +1,68 @@
 # < ------------- SHINY UI ------------- >
+light <- bs_theme()
+dark <- bs_theme(bg = "black", fg = "white", primary = "purple")
 
-fluidPage(
-  useShinyjs(), # Include shinyjs
-  titlePanel("Geologic Age Visualization"),
-  sidebarLayout(
-    # -----------Sidebar -------------- #
-    sidebarPanel(
-      pickerInput("model", "Predict unknown ages:",
-                  choices = model_names,
-                  selected = NULL,
-                  multiple = TRUE,
-                  options = pickerOptions(maxOptions = 1))
+page_sidebar(
+  title = "Geologic Age Visualization",
+  
+  # ----------Sidebar------------- #
+  sidebar = sidebar(
+    accordion(
+      #------------ Accordion: Graph Options ----------------#
+      accordion_panel(
+        "Graph Options",
+        sliderInput("polyopacity", "Individual Polygon Opacity:", min = 0, max = 1,
+                    value = 0.1, step = 0.01)
       ),
-    # -----------Main Panel -------------- #
-    mainPanel(
-      leafletOutput("map"),
-      sliderTextInput("age_slider", "", #min = min(df_point_csv$Age), max = max(df_point_csv$Age),
-                 choices = age_slider_scale,
-                 selected = max(df_point_csv$Age),
-                 #step = 1, 
-                 animate = animationOptions(interval = 250, loop = FALSE) )
+      #------------ Accordion: Model Options ----------------#
+      accordion_panel(
+        "Model Options",
+        pickerInput("model", "Predict unknown ages:",
+                    choices = model_names,
+                    selected = NULL,
+                    multiple = TRUE,
+                    options = pickerOptions(maxOptions = 1))
       )
     )
+  ), #### End sidebar
+  leafletOutput("map"),
+  sliderTextInput("age_slider", "Age (Ma):", 
+                  choices = age_slider_scale,
+                  selected = prev_age,
+                  #step = 1, 
+                  animate = animationOptions(interval = 250, loop = FALSE), )
+  
 )
+
+
+
+# fluidPage(
+#   useShinyjs(), # Include shinyjs
+#   #theme = light,
+#   titlePanel("Geologic Age Visualization",
+#              #checkboxInput("dark_mode", "Dark mode")
+#              ),
+#   sidebarLayout(
+#     # -----------Sidebar -------------- #
+#     sidebarPanel(
+#       h3("Graph Options"),
+#       sliderInput("poly-opacity", "Individual Polygon Opacity:", min = 0, max = 1,
+#                   value = 0.1, step = 0.01),
+#       h3("Model Options"),
+#       pickerInput("model", "Predict unknown ages:",
+#                   choices = model_names,
+#                   selected = NULL,
+#                   multiple = TRUE,
+#                   options = pickerOptions(maxOptions = 1))
+#       ),
+#     # -----------Main Panel -------------- #
+#     mainPanel(
+#       leafletOutput("map"),
+#       sliderTextInput("age_slider", "", 
+#                  choices = age_slider_scale,
+#                  selected = max(df_point_csv$Age),
+#                  #step = 1, 
+#                  animate = animationOptions(interval = 250, loop = FALSE), ),
+#       )
+#     )
+# )
